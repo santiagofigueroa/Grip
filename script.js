f09c8e6614646d518c94d3aad98531a08079acd0
Santiago <santiago_figueroa_@hotmail.com>
2020-07-30 10:52:38 +0100

    Q3 working with different implementation. Json Data bug fix .

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 
-setup();
-
-
-var jsonRec = jsonData();
-console.log(jsonRec);
-showMessage(jsonRec);
-
-// Will return. 
+// Q3 - Returns users  
 function getUsers(records,  action ,  start_time, end_time ){
+    var tmpRec  =  {};
+    tmpRec = records;
+    console.log(tmpRec);
+    var result = [] ;
 
+    for(key in tmpRec){
+        console.log(tmpRec[key].user_id);
+        if(tmpRec.hasOwnProperty(key)){
+            if(tmpRec[key].date_actioned >= start_time && 
+                tmpRec[key].date_actioned <= end_time){
 
+             if(tmpRec[key].action == action){
+                console.log(tmpRec[key].user_id);
+                // Add the correct user package.
+                result.push(tmpRec[key].user_id);
+                
+            }
+        }
+    }
 
+    }
+    showMessage(result);
+    return result;
 }
 
 
-//getPlaybackTime(1, records)//will return 310
+// Q4 - //getPlaybackTime(1, records)//will return 310
+
+function getPlaybackTime(user_id, records){
+
+
+}
--- utils.js
+++ utils.js
@@ -1,8 +1,63 @@
-// Helps displaying to HTML in div tags.
-function showMessage(message){
-document.getElementById("q3").innerHTML = message;
+//
+var data = [
+{
+    "user_id": 1,
+    "device":"Windows 10",
+    "action":"start",
+    "date_actioned":100
+},
+{
+    "user_id": 2,
+    "device":"OSX 15",
+    "action":"start",
+    "date_actioned":200
+},{
+    "user_id": 1,
+    "device":"iPhone 8s",
+    "action":"start",
+    "date_actioned":250
+},
+{
+    "user_id": 1,
+    "device":"Windows 10",
+    "action":"stop",
+    "date_actioned":370
+},
+{
+    "user_id": 1,
+    "device":"iPhone 8s",
+    "action":"stop",
+    "date_actioned":410
+},
+{
+    "user_id": 2,
+    "device":"OSX 15.4",
+    "action":"stop",
+    "date_actioned":490
+},
+{
+    "user_id": 3,
+    "device":"Android 9.1",
+    "action":"start",
+    "date_actioned":700
 }
-let data = [];
+
+];
+// Valid JSON
+function validJSON(jsonString){
+    try{
+        if(typeof jsonString != 'string'){
+            return false;
+       }
+        var st = JSON.parse(jsonString);   
+        return true           
+    }catch(error){
+        console.log(error);
+        return false;     
+    }
+    
+}
+
 function setup(){
     var xhp  = new XMLHttpRequest();
 	
@@ -13,23 +68,36 @@ function setup(){
        xhp.onreadystatechange = function (){
         
         if (xhp.readyState == 4 && xhp.status === 200){
-            console.log("Success "); 
-             data = JSON.parse(xhp.responseText);
+            console.log("Success ");
+             
+             var data1 = JSON.parse(xhp.responseText);
             // Making sure the data is fine to proccess
-            console.log(data);   
-            jsonData(data);   
+            console.log(validJSON(data1));
+            console.log(data1); 
+            document.getElementById("con").innerHTML = "Status is: " + xhp.status;
+            //jsonData();
+            return data1;
         
         } else {
-            console.log("Fail");    
+            console.log("Fail"); 
+            document.getElementById("con").innerHTML = "Status is: " + xhp.status;
+            return "No data found";
         }
-        document.getElementById("con").innerHTML = "Status is: " + xhp.status;
+
+        
+        
        
        }//  End of function. 
 
 }
 
-function jsonData(){
-    console.log(data);
-    return data;
-
+//Implementation on records
+//data = setup();
+// Use to return data to other Parts of the program 
+console.log(data);
+getUsers(data,"start" ,700 ,900);
+console.log();
+// Helps displaying to HTML in div tags.
+function showMessage(message){
+    document.getElementById("q3").innerHTML = message;
 }
